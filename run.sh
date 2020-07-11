4c7c8f448d7d49665443a8ca74cc69a4b8463c4a
Jared Allard <jaredallard@outlook.com>
2020-07-11 11:12:22 -0700

    chore: fixes

--- run.sh
+++ run.sh
@@ -1,22 +1,25 @@
 #!/usr/bin/env bash
+#
+# run.sh loads the wireguard kernel module and it's dependencies, if needed
 
 OS_VERSION=$(awk '{ print $2 }' <<< "$BALENA_HOST_OS_VERSION")
 modPath="output/wireguard-linux-compat/src_${BALENA_DEVICE_TYPE}_${OS_VERSION}.prod_from_src/wireguard.ko"
-echo "OS Version is $OS_VERSION"
+echo " :: OS Version is $OS_VERSION"
 
 if lsmod | grep wireguard >/dev/null 2>&1; then
-	echo "skipping wireguard load, already loaded"
-	echo "Loaded Wireguard version: $(cat /sys/module/wireguard/version 2>/dev/null || echo "failed to obtain version")"
+	echo " :: Skipping wireguard load, already loaded"
+	echo " :: Loaded Wireguard version: $(cat /sys/module/wireguard/version 2>/dev/null || echo "failed to obtain version")"
 else
-	echo "loading pre-req modules"
-	modprobe udp_tunnel ip6_udp_tunnel || echo "Error: failed to load wireguard dependencies"
+	echo " :: Loading pre-req modules"
+	modprobe -a udp_tunnel ip6_udp_tunnel
 
-	echo "loading wireguard"
+	echo " :: Loading wireguard"
 	modinfo "$modPath"
 	if ! insmod "$modPath"; then
 	 	dmesg | grep wireguard
 	fi
 fi
 
-# now we wait forever, allows debugging the container
-exec sleep infinity
+if [[ -n "$DEBUG_MODE" ]]; then
+  exec sleep infinity
+fi
